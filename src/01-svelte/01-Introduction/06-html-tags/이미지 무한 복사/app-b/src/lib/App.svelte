49891ec5b5dffe69ff17f91c9166ca10ca722d0d
myrlagksruf <myrlagksruf@gmail.com>
2023-02-05 00:41:44 +0900

    app-b 추가

---
+++ src/01-svelte/01-Introduction/06-html-tags/이미지 무한 복사/app-b/src/lib/App.svelte
@@ -0,0 +1,21 @@
+<script>
+	let html = `<img src='/img.gif' style="width:10%;height:10%;">`;
+	let str = '';
+	let count = 0;
+	let setT = 0
+</script>
+<div>
+	{@html str}
+</div>
+<style>
+	:global(main){
+		padding:0 !important;
+	}
+	div{
+		width:100vw;
+		height:100vh;
+		display: flex;
+		flex-wrap: wrap;
+		align-content: flex-start;
+	}
+</style>
