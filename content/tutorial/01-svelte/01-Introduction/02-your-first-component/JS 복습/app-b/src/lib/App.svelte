2fe86174ff411b0ef8db2e0bffeb810209c0fe4d
dgkim2008 <dongg9318@gmail.com>
2023-02-07 23:48:37 +0900

    컴포넌트 만들기 숙제

--- content/tutorial/01-svelte/01-Introduction/02-your-first-component/JS 복습/app-b/src/lib/App.svelte
+++ content/tutorial/01-svelte/01-Introduction/02-your-first-component/JS 복습/app-b/src/lib/App.svelte
@@ -1,9 +1,24 @@
 <script>
     let str = 'hello world';
+
+    let x = 12345 + 54321 * 12345 / 54321
+
+    let change = str.replaceAll('l', '1');
+
+    let seven = '';
+
+    for(let i = 1; i <= 100; i++) {
+        if(i % 7 === 0 && i === 7) {
+            seven = `${i}`;
+        }
+        else if(i % 7 === 0) {
+            seven = `${seven}, ${i}`;
+        }
+    }
 </script>
 
 <ol>
-    <li>12345 + 54321 * 12345 / 54321 의 답은 ? 입니다.</li>
-    <li>hello wolrd에서 l을 모두 1로 바꾼 문자열은 ? 입니다.</li>
-    <li>1에서 100까지 7의 배수는 ? 입니다.</li>
+    <li>12345 + 54321 * 12345 / 54321 의 답은 {x} 입니다.</li>
+    <li>hello wolrd에서 l을 모두 1로 바꾼 문자열은 {change} 입니다.</li>
+    <li>1에서 100까지 7의 배수는 {seven} 입니다.</li>
 </ol>
