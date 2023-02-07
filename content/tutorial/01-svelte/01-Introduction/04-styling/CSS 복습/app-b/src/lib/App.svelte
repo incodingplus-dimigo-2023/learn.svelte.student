f4a395dba83fd42e9fa0989146e34080db2af9ad
Junhyeok08 <wnsgur_081106@naver.com>
2023-02-08 01:13:46 +0900

    숙제 3 제출

--- content/tutorial/01-svelte/01-Introduction/04-styling/CSS 복습/app-b/src/lib/App.svelte
+++ content/tutorial/01-svelte/01-Introduction/04-styling/CSS 복습/app-b/src/lib/App.svelte
@@ -1,46 +1,53 @@
 <div class="container">
-	<h1>Hello, nest!</h1>
-	<div>
-		A progressive Node.js framework for building efficient, reliable and scalable server-side
-		applications.
+	<h1 class="hello">Hello, nest!</h1>
+	<div class="sentence">
+	  A progressive Node.js framework for building efficient, reliable and scalable server-side
+	  applications.
 	</div>
 	<div class="btn-container">
-		<button class="doc">Documentation</button>
-		<button class="git">Source code</button>
+	  <button class="doc">Documentation</button>
+	  <button class="git">Source code</button>
 	</div>
-</div>
-
-<style>
+  </div>
+  
+  <style>
 	.container {
-	
+	  background-color: black;
+	  height: 200px;
+	  padding: 50px;
 	}
-
+  
 	.btn-container {
-		
+	  padding: 50px;
 	}
-
+  
 	button {
-		border-style: solid;
-		border-width: 2px;
-		border-radius: 30px;
-		text-align: center;
-		padding: 14px 28px;
-		min-width: 230px;
-		color: white;
-		margin: 0px 30px;
+	  border-style: solid;
+	  border-width: 2px;
+	  border-radius: 30px;
+	  text-align: center;
+	  padding: 14px 28px;
+	  min-width: 230px;
+	  color: white;
+	  margin: 0px 30px;
 	}
-
+  
 	.doc {
-
+	  background-color: #ea2845;
+	  border-color: #ea2845;
 	}
+  
 	.doc:hover {
-
+	  background-color: black;
 	}
-
+  
 	.git {
-	
+	  background-color: black;
+	  border-color: white;
 	}
+  
 	.git:hover {
-		
+	  border-color: #ea2845;
+	  color: #ea2845;
 	}
-</style>
+  </style>
