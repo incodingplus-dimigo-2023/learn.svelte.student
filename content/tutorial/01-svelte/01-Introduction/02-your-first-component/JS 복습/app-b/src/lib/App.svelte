b39da4d25805b96f8f11161e319eb05ad167b211
Haerin <beal2002@naver.com>
2023-02-09 22:43:38 +0900

    Js 복습

--- content/tutorial/01-svelte/01-Introduction/02-your-first-component/JS 복습/app-b/src/lib/App.svelte
+++ content/tutorial/01-svelte/01-Introduction/02-your-first-component/JS 복습/app-b/src/lib/App.svelte
@@ -1,9 +1,19 @@
 <script>
     let str = 'hello world';
+    let one = 12345 + 54321 * 12345 / 54321
+    let two = str.replaceAll('l', '1');
+    let three = [];
+    let count =0;
+    for(let i=7; i<=100; i+=7){
+        
+        three[count]=i;
+        count+=1;
+    }
+    let four = three;
 </script>
 
 <ol>
-    <li>12345 + 54321 * 12345 / 54321 의 답은 ? 입니다.</li>
-    <li>hello wolrd에서 l을 모두 1로 바꾼 문자열은 ? 입니다.</li>
-    <li>1에서 100까지 7의 배수는 ? 입니다.</li>
+    <li>12345 + 54321 * 12345 / 54321 의 답은 {one} 입니다.</li>
+    <li>hello wolrd에서 l을 모두 1로 바꾼 문자열은 {two} 입니다.</li>
+    <li>1에서 100까지 7의 배수는 {four} 입니다.</li>
 </ol>
--- content/tutorial/01-svelte/01-Introduction/03-dynamic-attributes/하이퍼링크/app-b/src/lib/App.svelte
+++ content/tutorial/01-svelte/01-Introduction/03-dynamic-attributes/하이퍼링크/app-b/src/lib/App.svelte
@@ -6,6 +6,6 @@
     const target = "_blank"
 </script>
 
-<a>구글</a>
-<a>네이버</a>
+<a >구글</a>
+<a >네이버</a>
 <a>인코딩학원</a>
