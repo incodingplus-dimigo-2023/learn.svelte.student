b5cffec8c2fd5b08e1b90885cf8b83ead1139fd6
Junhyeok08 <wnsgur_081106@naver.com>
2023-02-17 00:09:01 +0900

    숙제  제출

--- content/tutorial/01-svelte/02-reactivity/03-reactive-statements/구구단/app-b/src/lib/App.svelte
+++ content/tutorial/01-svelte/02-reactivity/03-reactive-statements/구구단/app-b/src/lib/App.svelte
@@ -3,11 +3,18 @@
 	let multiplicationTable = "";
 
 	$: {
-		
+		multiplicationTable = '';
+		if(number > 0){
+		for (let i = 1 ; i <= 9; i ++){
+			multiplicationTable += `${number} * ${i} = ${number * i} \n`
+			console.log(multiplicationTable)
+		}
+		}
 	}
 
 	const setNumber = (num) => {
-		number = num
+		number = num;
+		console.log(number)
 	}
 </script>
 
@@ -15,5 +22,7 @@
 	<button on:click={() => setNumber(3)}>3단</button>
 	<button on:click={() => setNumber(6)}>6단</button>
 	<button on:click={() => setNumber(9)}>9단</button>
+	<br>
+	{multiplicationTable}
 </div>
 <div></div>
