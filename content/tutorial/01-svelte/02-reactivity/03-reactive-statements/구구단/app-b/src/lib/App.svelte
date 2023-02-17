771395b7d0190155331c5ee25bb2151b61fb0173
CaptinKKang <milkway1128@gmail.com>
2023-02-17 22:44:18 +0900

    스벨트 반응성 2번째까지 완료

--- content/tutorial/01-svelte/02-reactivity/03-reactive-statements/구구단/app-b/src/lib/App.svelte
+++ content/tutorial/01-svelte/02-reactivity/03-reactive-statements/구구단/app-b/src/lib/App.svelte
@@ -1,19 +1,31 @@
 <script>
 	let number = 0;
+	let click = 0;
 	let multiplicationTable = "";
-
+	const con = document.querySelector('.con');
+	let arr = [];
+	const divs = [];
+	for(let i = 1; i < 10; i++){
+		arr.push(i);
+		let div = document.createElement("div");
+		divs.push(div);
+		con.appendChild(div);
+	}
 	$: {
+		for(let i = 1; i < 10; i++){
+			con.children[i-1].innerHTML = `${number}*${i}=${number*i}`
+		}
 		
 	}
 
 	const setNumber = (num) => {
-		number = num
+		click++
+		number = num;
 	}
 </script>
-
 <div>
 	<button on:click={() => setNumber(3)}>3단</button>
 	<button on:click={() => setNumber(6)}>6단</button>
 	<button on:click={() => setNumber(9)}>9단</button>
 </div>
-<div></div>
+<div class="con"></div>
