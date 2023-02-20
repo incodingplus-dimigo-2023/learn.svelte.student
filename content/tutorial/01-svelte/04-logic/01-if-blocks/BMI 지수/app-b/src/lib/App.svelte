f04d713fbbd6c14243e80ce1298e44681d9dfb88
myrlagksruf <myrlagksruf@gmail.com>
2023-02-20 22:06:58 +0900

    숙제 추가

---
+++ content/tutorial/01-svelte/04-logic/01-if-blocks/BMI 지수/app-b/src/lib/App.svelte
@@ -0,0 +1,15 @@
+<script>
+	import Component from "./Component.svelte";
+
+	let arr = [{
+		weight: 75,
+		height: 185
+	}, {
+		weight: 80,
+		height: 170
+	}, {
+		weight: 50,
+		height: 192
+	}]
+</script>
+
