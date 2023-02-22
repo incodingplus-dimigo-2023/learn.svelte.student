320fe69bea8a229bbb0cd81452488079424ca658
Rumi <lumee.yi@gmail.com>
2023-02-22 22:55:17 +0900

    페이지 이동 품

--- content/tutorial/01-svelte/04-logic/03-else-if-blocks/페이지 이동/app-b/src/lib/App.svelte
+++ content/tutorial/01-svelte/04-logic/03-else-if-blocks/페이지 이동/app-b/src/lib/App.svelte
@@ -1,31 +1,44 @@
 <script>
-	let page = 1
+	let page = 1;
 
+	function minus() {
+		page = page - 1;
+	}
+	function plus() {
+		page = page + 1;
+	}
+	function one() {
+		page = 1;
+	}
 </script>
 
-
+{#if page == 1}
 	<div>
 		<h1>첫 번째 페이지입니다.</h1>
-		<div></div>
+		<div />
 	</div>
-
+{:else if page == 2}
 	<div>
 		<h1>두 번째 페이지입니다.</h1>
-		<div></div>
+		<div />
 	</div>
-
+{:else if page == 3}
+	<div>
+		<h1>마지막 페이지입니다.</h1>
+		<div>감사합니다.</div>
+	</div>
+{:else if page > 3}
+	<div>
+		<h1>잘못된 페이지입니다.</h1>
+		<button on:click={one}>1 페이지로 돌아가기</button>
+	</div>
+{:else if page < 1}
+	<div>
+		<h1>잘못된 페이지입니다.</h1>
+		<button on:click={one}>1 페이지로 돌아가기</button>
+	</div>
+{/if}
 <div>
-	<h1>마지막 페이지입니다.</h1>
-	<div>감사합니다.</div>
-</div>
-
-<div>
-	<h1>잘못된 페이지입니다.</h1>
-	<button>1 페이지로 돌아가기</button>
-</div>
-
-
-<div>
-	<button>이전 페이지로</button>
-	<button>다음 페이지로</button>
+	<button on:click={minus}>이전 페이지로</button>
+	<button on:click={plus}>다음 페이지로</button>
 </div>
