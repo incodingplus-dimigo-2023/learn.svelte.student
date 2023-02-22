7ebe42c083daa2799256fcc3db1da4ac4e04fa95
Rumi <lumee.yi@gmail.com>
2023-02-22 22:37:50 +0900

    로그인

--- content/tutorial/01-svelte/04-logic/02-else-blocks/로그인/app-b/src/lib/App.svelte
+++ content/tutorial/01-svelte/04-logic/02-else-blocks/로그인/app-b/src/lib/App.svelte
@@ -1,14 +1,21 @@
 <script>
-	let isTeacherMode = false
+	let isTeacherMode = false;
+	function toggle() {
+		isTeacherMode = !isTeacherMode;
+	}
 </script>
 
-<input type="password" placeholder="선생님 코드를 입력해주세요">	
-<input type="text" placeholder="아이디를 입력해주세요">
-<input type="password" placeholder="비밀번호를 입력해주세요">	
-<div>
-	선생님 모드: 
-</div>
+{#if isTeacherMode}
+	<input type="password" placeholder="선생님 코드를 입력해주세요" />
+	<div on:click={toggle}> 선생님 모드: {isTeacherMode} </div>
+{:else}
+	<input type="text" placeholder="아이디를 입력해주세요" />
+	<input type="password" placeholder="비밀번호를 입력해주세요" />
+	<div on:click={toggle}> 선생님 모드: {isTeacherMode} </div>
+{/if}
 
+<!-- 
+<div>선생님 모드:</div> -->
 <style>
 	input {
 		display: block;
