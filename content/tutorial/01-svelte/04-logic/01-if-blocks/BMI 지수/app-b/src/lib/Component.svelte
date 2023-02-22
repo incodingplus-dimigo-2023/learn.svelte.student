23242126ad5931769aed81300d9f977e22931bcc
Rumi <lumee.yi@gmail.com>
2023-02-22 22:25:10 +0900

    BMI지수 품

--- content/tutorial/01-svelte/04-logic/01-if-blocks/BMI 지수/app-b/src/lib/Component.svelte
+++ content/tutorial/01-svelte/04-logic/01-if-blocks/BMI 지수/app-b/src/lib/Component.svelte
@@ -4,19 +4,28 @@
 
     let BMI;
     let state;
+    let cheak
 
     $: {
-        let bmi = weight / (height * height) *10000;
-        BMI = bmi;
+        let bmi = (weight / (height * height)) * 10000;
         if (bmi > 25.0) {
             let fat = "비만";
+            state = fat;
+            cheak= "red";
         } else if (bmi >= 23.0 && bmi <= 24.9) {
             let nomalfat = "과체중";
+            state = nomalfat;
+            cheak= "red";
         } else if (bmi >= 18.5 && bmi <= 22.5) {
             let nomal = "정상";
+            state = nomal;
+            cheak= "black";
         } else if (bmi <= 22.5) {
             let less = "저체중";
+            state = less;
+            cheak= "red";
         }
+        BMI = bmi;
     }
 </script>
 
@@ -24,6 +33,7 @@
     <div>신장 : {height}</div>
     <div>체중 : {weight}</div>
     <div>BMI : {BMI}</div>
+    <div style="color: {cheak};">{state}</div>
 </div>
 
 <style>
