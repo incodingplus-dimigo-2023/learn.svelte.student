f3d276326882cf56a92a0e6558bec857a75d0aab
Rumi <lumee.yi@gmail.com>
2023-02-23 23:20:44 +0900

    캐랙터 소환 품

--- content/tutorial/01-svelte/04-logic/04-each-blocks/캐릭터 소환/app-b/src/lib/App.svelte
+++ content/tutorial/01-svelte/04-logic/04-each-blocks/캐릭터 소환/app-b/src/lib/App.svelte
@@ -1,33 +1,46 @@
 <script>
-	let characters = []
-
+	let characters = [];
+	let count = 0;
+	let CNT;
+	let HP;
+	let ATK;
+	let DEF;
+	let STARMARK;
+	let starnum;
+	var stars = ["", "★", "★★", "★★★", "★★★★", "★★★★★", "★★★★★★"];
+	let container;
 	const createCharacter = () => {
-		let HP;
-		let ATK;
-		let DEF;
-		let STAR;
+		CNT = count + 1;
+		HP = Math.floor(Math.random() * 201);
+		ATK = Math.floor(Math.random() * 201);
+		DEF = Math.floor(Math.random() * 201);
+		starnum = Math.floor((HP + ATK + DEF) / 100);
+		STARMARK = stars[starnum];
 
-		return {
+		characters[count] = {
+			CNT,
 			HP,
 			ATK,
 			DEF,
-			STAR
-		}
-	}
+			STARMARK,
+		};
+		count++;
+	};
 
-	const spawn = () => {
-		
-	}
+	// const spawn = () => {
+
+	// };
 </script>
 
-<button>소환</button>
+<button on:click={createCharacter}>소환</button>
 <div class="container">
-	<div class="character">
-		<h1></h1>
-		<div></div>
-		<div></div>
-		<div></div>
-		<div></div>
-	</div>
+	{#each characters as { STARMARK, HP, ATK, DEF }, i}
+		<div class="character">
+			<h1>{i + 1}번째 캐릭터</h1>
+			<div>체력 : {HP}</div>
+			<div>공격력 : {ATK}</div>
+			<div>방어력 : {DEF}</div>
+			<div>등급 : {STARMARK}</div>
+		</div>
+	{/each}
 </div>
-
