cb16c7a665e57c0a536d2105dec8408e97487a85
dgkim2008 <dongg9318@gmail.com>
2023-02-24 13:25:45 +0900

    페이지 이동 숙제제출

--- content/tutorial/01-svelte/04-logic/03-else-if-blocks/페이지 이동/app-b/src/lib/App.svelte
+++ content/tutorial/01-svelte/04-logic/03-else-if-blocks/페이지 이동/app-b/src/lib/App.svelte
@@ -1,31 +1,42 @@
 <script>
 	let page = 1
-
 </script>
 
+	{#if page === 1}
+		<div>
+			<h1>첫 번째 페이지입니다.</h1>
+			<div></div>
+			<div>
+				<button on:click={() => page--}>이전 페이지로</button>
+				<button on:click={() => page++}>다음 페이지로</button>
+			</div>
+		</div>
+		{:else if page === 2} 
+		<div>
+			<h1>두 번째 페이지입니다.</h1>
+			<div></div>
+			<div>
+				<button on:click={() => page--}>이전 페이지로</button>
+				<button on:click={() => page++}>다음 페이지로</button>
+			</div>
+		</div>
+		{:else if page === 3} 
+		<div>
+			<h1>마지막 페이지입니다.</h1>
+			<div>감사합니다.</div>
+			<div>
+				<button on:click={() => page--}>이전 페이지로</button>
+				<button on:click={() => page++}>다음 페이지로</button>
+			</div>
+		</div>
+		{:else} 
+		<div>
+			<h1>잘못된 페이지입니다.</h1>
+			<button on:click={() => page = 1}>1 페이지로 돌아가기</button>
+		</div>
 
-	<div>
-		<h1>첫 번째 페이지입니다.</h1>
-		<div></div>
-	</div>
-
-	<div>
-		<h1>두 번째 페이지입니다.</h1>
-		<div></div>
-	</div>
-
-<div>
-	<h1>마지막 페이지입니다.</h1>
-	<div>감사합니다.</div>
-</div>
-
-<div>
-	<h1>잘못된 페이지입니다.</h1>
-	<button>1 페이지로 돌아가기</button>
-</div>
+	{/if}
 
 
-<div>
-	<button>이전 페이지로</button>
-	<button>다음 페이지로</button>
-</div>
+
+
