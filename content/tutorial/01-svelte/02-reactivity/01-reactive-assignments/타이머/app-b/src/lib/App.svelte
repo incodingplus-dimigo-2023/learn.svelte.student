d80cb48e710d5072894d422bc672688376061413
Haerin <beal2002@naver.com>
2023-02-25 15:29:58 +0900

    타이머

--- content/tutorial/01-svelte/02-reactivity/01-reactive-assignments/타이머/app-b/src/lib/App.svelte
+++ content/tutorial/01-svelte/02-reactivity/01-reactive-assignments/타이머/app-b/src/lib/App.svelte
@@ -1,26 +1,38 @@
 <script>
+    import { time_ranges_to_array } from "svelte/internal";
+
+
 	let number = 5;
 	let interval = null;
 	let st = false;
 
 	const reset = () => {
-		number=5;
+		number = 5;
 	};
 	const pause = () => {
-		st = false
+		st = false;
 	};
 	const start = () => {
 		st = true;
+		time();
 	};
-	$: {{if (st) {
-		setInterval(() => {
-			console.log(number);
-			number -= 1;
-		}, 1000);
-	};
-	if(number<=0){
-		alert('종료');
-	};}}
+	// $: {
+	// 	{
+			function time () {
+				if (st) {
+				//interval = setInterval(() => {
+					number = number - 1;
+					if (number === 0) {
+						alert("종료");
+						return;
+						//clearInterval(interval);
+					}
+				//}, 1000);
+				setTimeout(time, 1000);
+			}
+		}
+//	}
+//}
 </script>
 
 <div>
