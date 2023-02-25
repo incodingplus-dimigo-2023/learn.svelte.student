821ba82473c32bf6c7f2bdb1071eac155a2615c6
dgkim2008 <dongg9318@gmail.com>
2023-02-25 16:29:42 +0900

    캐릭터 소환

--- content/tutorial/01-svelte/04-logic/04-each-blocks/캐릭터 소환/app-b/src/lib/App.svelte
+++ content/tutorial/01-svelte/04-logic/04-each-blocks/캐릭터 소환/app-b/src/lib/App.svelte
@@ -1,12 +1,22 @@
 <script>
+    import { each } from "svelte/internal";
+
+	/**
+	 * @type {{	
+	  		HP:number,	
+			ATK:number,
+			DEF:number,
+			STAR:number
+		}[]}
+	 */
 	let characters = []
 
-
 	const createCharacter = () => {
-		let HP = Math.floor(Math.random() * 200) + 1;
-		let ATK = Math.floor(Math.random() * 200) + 1;
-		let DEF = Math.floor(Math.random() * 200) + 1;
-		let STAR = Math.floor(Math.random() * 200) + 1;
+		let stat = () => Math.floor(Math.random() * 201);
+		let HP = stat();
+		let ATK = stat();
+		let DEF = stat();
+		let STAR = Math.floor((HP + ATK + DEF) / 100);
 		
 		return {
 			HP,	
@@ -17,18 +27,21 @@
 	}
 	
 	const spawn = () => {
-		
+		let char = createCharacter();
+		characters = [...characters, char];
 	}
 </script>
 
-<button>소환</button>
-<div class="container">
+<button on:click={spawn}>소환</button>
+<div class="container">	
+	{#each characters as char, ind}
 	<div class="character">
-		<h1></h1>
-		<div></div>
-		<div></div>
-		<div></div>
-		<div></div>
+		<h1>{ind + 1}번째 캐릭터</h1>	
+		<div>체력 : {char.HP}</div>
+		<div>공격력: {char.ATK}</div>
+		<div>방어력: {char.DEF}</div>
+		<div>등급 : {'★'.repeat(char.STAR + 1)}</div>
 	</div>
+	{/each}
 </div>
 
