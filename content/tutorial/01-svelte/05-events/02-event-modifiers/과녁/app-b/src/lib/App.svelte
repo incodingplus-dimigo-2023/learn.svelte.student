66e85c7f7c126973824abfb04e40efaa9c9b0c54
Haerin <beal2002@naver.com>
2023-03-02 22:41:13 +0900

    과녁

--- content/tutorial/01-svelte/05-events/02-event-modifiers/과녁/app-b/src/lib/App.svelte
+++ content/tutorial/01-svelte/05-events/02-event-modifiers/과녁/app-b/src/lib/App.svelte
@@ -1,25 +1,46 @@
 <script>
 	const colors = ["red", "blue", "green"]
 	let arr = ["", "", ""]
+
 </script>
 
 <div 
 	class={arr[0]} 
-	on:click={() => arr[0] = colors[0]} 
-	on:contextmenu={() => arr[0] = ""}
+	on:click|stopPropagation={() => arr[0] = colors[0]} 
+	on:contextmenu|preventDefault|stopPropagation={() => arr[0] = ""}
 >
 	<div 
 		class={arr[1]} 
-		on:click={() => arr[1] = colors[1]} 
-		on:contextmenu={() => arr[1] = ""}
+		on:click|stopPropagation={() => arr[1] = colors[1]} 
+		on:contextmenu|preventDefault|stopPropagation={() => arr[1] = ""}
 	>
 		<div 
 			class={arr[2]} 
-			on:click={() => arr[2] = colors[2]} 
-			on:contextmenu={() => arr[2] = ""}>
+			on:click|stopPropagation={() => arr[2] = colors[2]} 
+			on:contextmenu|preventDefault|stopPropagation={() => arr[2] = ""}>
 		</div>
 	</div>
 </div>
+
+
+<style>
+	div {
+		padding: 20px;
+		width: fit-content;
+		border-radius: 9999px;
+		border: 1px solid black;
+		background-color: white;
+	}
+	.red {
+		background-color: red;
+	}
+	.green {
+		background-color: green;
+	}
+	.blue {
+		background-color: blue;
+	}
+</style>
 
 
 <style>
