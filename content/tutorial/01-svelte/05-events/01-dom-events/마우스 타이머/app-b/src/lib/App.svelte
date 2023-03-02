8c558291322ab7a045d4ef2ebc5b9fe544f46aec
Haerin <beal2002@naver.com>
2023-03-02 22:34:17 +0900

    마우스 타이머

--- content/tutorial/01-svelte/05-events/01-dom-events/마우스 타이머/app-b/src/lib/App.svelte
+++ content/tutorial/01-svelte/05-events/01-dom-events/마우스 타이머/app-b/src/lib/App.svelte
@@ -1,9 +1,25 @@
 <script>
 	let time = 0
 	let isMouseDown = false
+  
+	$: if(isMouseDown){
+		setTimeout(seconde, 1);
+    function seconde(){
+      time+=1;
+    }
+	};
+ 
+	
+  function down() {
+    isMouseDown = true
+
+  }
+  function up() {
+    isMouseDown = false
+  }
 </script>
 
-<div>
+<div on:mousedown={down} on:mouseup={up}>
 	{isMouseDown ? "마우스를 누르는 중입니다." : `총 ${time / 1000}초 눌렀습니다.`}
 </div>
 
