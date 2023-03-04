4d43122d51851b23585eab7071776e1f6a8fd64b
dgkim2008 <dongg9318@gmail.com>
2023-03-04 15:26:13 +0900

    마우스 타이머 숙제 제출

--- content/tutorial/01-svelte/05-events/01-dom-events/마우스 타이머/app-b/src/lib/App.svelte
+++ content/tutorial/01-svelte/05-events/01-dom-events/마우스 타이머/app-b/src/lib/App.svelte
@@ -1,10 +1,24 @@
 <script>
 	let time = 0
+	let time_first
+	let time_last
 	let isMouseDown = false
+
+	
 </script>
 
-<div>
-	{isMouseDown ? "마우스를 누르는 중입니다." : `총 ${time / 1000}초 눌렀습니다.`}
+<div on:mousedown={
+	e => {
+		time_first = Date.now();
+		isMouseDown = true;
+	}
+} on:mouseup={
+	e => {
+		time_last = Date.now();
+		isMouseDown = false;
+	}
+}>		
+	{isMouseDown ? "마우스를 누르는 중입니다." : `총 ${(Number(time_last) - Number(time_first)) / 1000}초 눌렀습니다.`}
 </div>
 
 <style>
