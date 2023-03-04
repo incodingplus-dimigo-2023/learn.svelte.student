b3ad8272bb528c2db924864b0c2a503b42f3ad90
Haerin <beal2002@naver.com>
2023-03-04 14:01:27 +0900

    페이지 이동

--- content/tutorial/01-svelte/04-logic/03-else-if-blocks/페이지 이동/app-b/src/lib/App.svelte
+++ content/tutorial/01-svelte/04-logic/03-else-if-blocks/페이지 이동/app-b/src/lib/App.svelte
@@ -1,31 +1,41 @@
 <script>
-	let page = 1
+	let page = 1;
 
+	function p() {
+		page += 1;
+	}
+	function m() {
+		page -= 1;
+	}
+	function reset() {
+		page = 1;
+	}
 </script>
 
-
+{#if page === 1}
 	<div>
 		<h1>첫 번째 페이지입니다.</h1>
-		<div></div>
+		<div />
 	</div>
-
+{:else if page === 2}
 	<div>
 		<h1>두 번째 페이지입니다.</h1>
-		<div></div>
+		<div />
 	</div>
-
-<div>
-	<h1>마지막 페이지입니다.</h1>
-	<div>감사합니다.</div>
-</div>
-
-<div>
-	<h1>잘못된 페이지입니다.</h1>
-	<button>1 페이지로 돌아가기</button>
-</div>
-
-
-<div>
-	<button>이전 페이지로</button>
-	<button>다음 페이지로</button>
-</div>
+{:else if page === 3}
+	<div>
+		<h1>마지막 페이지입니다.</h1>
+		<div>감사합니다.</div>
+	</div>
+{:else}
+	<div>
+		<h1>잘못된 페이지입니다.</h1>
+		<button on:click={reset}>1 페이지로 돌아가기</button>
+	</div>
+{/if}
+{#if page>=1 && page <= 3}
+	<div>
+		<button on:click={m}>이전 페이지로</button>
+		<button on:click={p}>다음 페이지로</button>
+	</div>
+{/if}
