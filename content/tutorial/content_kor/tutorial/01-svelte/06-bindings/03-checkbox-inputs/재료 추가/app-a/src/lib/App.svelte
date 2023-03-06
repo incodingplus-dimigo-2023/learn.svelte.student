8472c7d8cd0c99dfac2b602be989d9774c6b9082
myrlagksruf <myrlagksruf@gmail.com>
2023-03-06 14:29:40 +0900

    binding까지 숙제 추가

---
+++ content/tutorial/content_kor/tutorial/01-svelte/06-bindings/03-checkbox-inputs/재료 추가/app-a/src/lib/App.svelte
@@ -0,0 +1,34 @@
+<script>
+	let orders = [{
+		name: "패티",
+		price: 2000,
+		checked: false
+	}, {
+		name: "치즈",
+		price: 300,
+		checked: false
+	}, {
+		name: "베이컨",
+		price: 600,
+		checked: false
+	}, {
+		name: "통새우",
+		price: 300,
+		checked: false
+	}, {
+		name: "할라피뇨",
+		price: 400,
+		checked: false
+	}, {
+		name: "야채",
+		price: 400,
+		checked: false
+	}]
+
+	let totalPrice = 0
+</script>
+
+<div class="order">
+
+	<div>총 : {totalPrice}원</div>
+</div>
