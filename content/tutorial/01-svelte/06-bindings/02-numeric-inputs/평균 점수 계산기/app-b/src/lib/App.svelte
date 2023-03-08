a9f6b07e0f5b9c0823a16fe524ffcdff79ccfba7
dgkim2008 <dongg9318@gmail.com>
2023-03-08 22:36:10 +0900

    평균 점수 계산기

--- content/tutorial/01-svelte/06-bindings/02-numeric-inputs/평균 점수 계산기/app-b/src/lib/App.svelte
+++ content/tutorial/01-svelte/06-bindings/02-numeric-inputs/평균 점수 계산기/app-b/src/lib/App.svelte
@@ -12,8 +12,19 @@
 		score: 0
 	}]
 
-	let averageScore = 0
+	let averageScore = 0;
+
+	$: averageScore = (subjects[0].score + subjects[1].score + subjects[2].score) / 3
+
+	
+
 </script>
+
+{#each subjects as { name, score}, i}
+	<div>
+		<span>{name}</span> <input type="number" min=0 max={MAX_SCORE} bind:value={score}> <span>점</span> 
+	</div>
+{/each}
 
 <div>
 	<div>평균 {averageScore}점</div>
