0292806632e9536c1d67d54e3b653306388bba61
dgkim2008 <dongg9318@gmail.com>
2023-03-08 23:43:19 +0900

    재료 추가

--- content/tutorial/01-svelte/06-bindings/03-checkbox-inputs/재료 추가/app-b/src/lib/App.svelte
+++ content/tutorial/01-svelte/06-bindings/03-checkbox-inputs/재료 추가/app-b/src/lib/App.svelte
@@ -2,33 +2,45 @@
 	let orders = [{
 		name: "패티",
 		price: 2000,
-		checked: false
+		checked: -1
 	}, {
 		name: "치즈",
 		price: 300,
-		checked: false
+		checked: -1
 	}, {
 		name: "베이컨",
 		price: 600,
-		checked: false
+		checked: -1
 	}, {
 		name: "통새우",
 		price: 300,
-		checked: false
+		checked: -1
 	}, {
 		name: "할라피뇨",
 		price: 400,
-		checked: false
+		checked: -1
 	}, {
 		name: "야채",
 		price: 400,
-		checked: false
+		checked: -1
 	}]
+	let picks = []
+	let totalPrice = 0;
+	
+	
 
-	let totalPrice = 0
 </script>
 
 <div class="order">
+	{#each orders as {name, price, checked}, i}
+		<div>
+			<input type="checkbox" bind:group={picks} name="picks" value={i} on:input={() => {
+				checked = checked * -1;
+				totalPrice = totalPrice + (orders[i].price * checked * -1);
+
+			}}> <span>{name}</span> <span>₩{price}</span> 
+		</div>
+	{/each}
 
 	<div>총 : {totalPrice}원</div>
 </div>
