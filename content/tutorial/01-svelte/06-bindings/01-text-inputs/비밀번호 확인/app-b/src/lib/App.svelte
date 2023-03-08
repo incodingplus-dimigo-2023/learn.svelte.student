0cb2ef0e1683fab23da68e957953b9ee423c2230
dgkim2008 <dongg9318@gmail.com>
2023-03-08 22:04:38 +0900

    비밀번호 확인

--- content/tutorial/01-svelte/06-bindings/01-text-inputs/비밀번호 확인/app-b/src/lib/App.svelte
+++ content/tutorial/01-svelte/06-bindings/01-text-inputs/비밀번호 확인/app-b/src/lib/App.svelte
@@ -3,9 +3,23 @@
 	let password2;
 
 	let isMatched = false
+
+	$: if(password1 === password2 && password1 !== undefined && password2 !== undefined) {
+		isMatched = true
+		console.log(password1)
+	} else {
+		isMatched = false
+	}
 </script>
 
-<input type="password">
-<input type="password">
 
-<div></div>
+<input type="password" bind:value={password1}>
+<input type="password" bind:value={password2}>
+
+{#if isMatched}
+	<div>확인</div>
+	{:else}
+	<div>비밀번호가 일치하지 않습니다</div>
+
+{/if}
+
