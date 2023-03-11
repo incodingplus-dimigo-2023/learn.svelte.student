d2f05e0cd40ea746160dadf58174d9a38633fda3
dog898996 <dog898996@gmail.com>
2023-03-11 15:48:14 +0900

    부조리

--- content/tutorial/01-svelte/06-bindings/01-text-inputs/비밀번호 확인/app-b/src/lib/App.svelte
+++ content/tutorial/01-svelte/06-bindings/01-text-inputs/비밀번호 확인/app-b/src/lib/App.svelte
@@ -2,13 +2,11 @@
   let password1;
   let password2;
   let isMatched = false;
-  const check = () => {
-    isMatched = Boolean(password1 === password2);
-  };
+  $: isMatched = Boolean(password1 === password2);
 </script>
 
-<input type="password" bind:value={password1} on:input={check} />
-<input type="password" bind:value={password2} on:input={check} />
+<input type="password" bind:value={password1} />
+<input type="password" bind:value={password2} />
 
 <div>
   {#if isMatched === false}
