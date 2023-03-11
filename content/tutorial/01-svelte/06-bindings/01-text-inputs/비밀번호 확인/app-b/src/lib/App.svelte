80e18e912fc98f4e469b15d7904216496409e4fb
Junhyeok08 <wnsgur_081106@naver.com>
2023-03-11 15:57:31 +0900

    숙제  제출

--- content/tutorial/01-svelte/06-bindings/01-text-inputs/비밀번호 확인/app-b/src/lib/App.svelte
+++ content/tutorial/01-svelte/06-bindings/01-text-inputs/비밀번호 확인/app-b/src/lib/App.svelte
@@ -2,19 +2,16 @@
 	let password1;
 	let password2;
 
-	let wrong = '비밀번호가 일치하지 않습니다'
-	let correct = '확인'
-
-	let isMatched = false;
-	console.log(password1,password2)
-	
-
+	$: isMatched = password1 && password2 && password1 === password2;
 </script>
 
-<input type="password">
-<input type="password">
+<input type="password" bind:value={password1}>
+<input type="password" bind:value={password2}>
 
 {#if isMatched}
-<div></div>
+	<div>확인</div>
+{:else}
+ 	<div>비밀번호가 일치하지 않습니다</div>
 {/if}
+
 
--- content/tutorial/01-svelte/06-bindings/02-numeric-inputs/평균 점수 계산기/app-b/src/lib/App.svelte
+++ content/tutorial/01-svelte/06-bindings/02-numeric-inputs/평균 점수 계산기/app-b/src/lib/App.svelte
@@ -1,4 +1,4 @@
-<script>
+<!-- <script>
 	const MAX_SCORE = 100
 	
 	let subjects = [{
@@ -31,6 +31,37 @@
 		width: 80px;
 	}
 </style>
+
+
+
+ -->
+
+
+ <script>
+	const MAX_SCORE = 100
+	
+	let subjects = [{
+		name: "국어",
+		score: 0
+	}, {
+		name: "수학",
+		score: 0
+	}, {
+		name: "영어",
+		score: 0
+	}]
+
+	$: averageScore = subjects.reduce((a,v) => a + v.score, 0) / subjects.length;
+</script>
+
+<div>
+  {#each subjects as {name, score}}
+    <div>
+    {name} : <input type = "number" min={0} max ={100} step={1} bind:value={score}>
+    </div>
+  {/each}
+	<div>평균 {averageScore}점</div>
+</div>
 
 
 
