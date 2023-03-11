8b9eb00346c1858a720ba1f0e200b4f6642cbf8d
Rumi <lumee.yi@gmail.com>
2023-03-11 16:09:19 +0900

    재료 추가

--- content/tutorial/01-svelte/06-bindings/03-checkbox-inputs/재료 추가/app-b/src/lib/App.svelte
+++ content/tutorial/01-svelte/06-bindings/03-checkbox-inputs/재료 추가/app-b/src/lib/App.svelte
@@ -1,34 +1,48 @@
 <script>
-	let orders = [{
-		name: "패티",
-		price: 2000,
-		checked: false
-	}, {
-		name: "치즈",
-		price: 300,
-		checked: false
-	}, {
-		name: "베이컨",
-		price: 600,
-		checked: false
-	}, {
-		name: "통새우",
-		price: 300,
-		checked: false
-	}, {
-		name: "할라피뇨",
-		price: 400,
-		checked: false
-	}, {
-		name: "야채",
-		price: 400,
-		checked: false
-	}]
+	let orders = [
+		{
+			name: "패티",
+			price: 2000,
+			checked: false,
+		},
+		{
+			name: "치즈",
+			price: 300,
+			checked: false,
+		},
+		{
+			name: "베이컨",
+			price: 600,
+			checked: false,
+		},
+		{
+			name: "통새우",
+			price: 300,
+			checked: false,
+		},
+		{
+			name: "할라피뇨",
+			price: 400,
+			checked: false,
+		},
+		{
+			name: "야채",
+			price: 400,
+			checked: false,
+		},
+	];
 
-	let totalPrice = 0
+	$: totalPrice = orders
+		.filter((v) => v.checked)
+		.reduce((a, v) => a + v.price, 0);
 </script>
 
 <div class="order">
-
+	{#each orders as { name, price, checked }, ind}
+		<div>
+			<input id="order-{ind}" type="checkbox" bind:checked />
+			{name} ₩{price}
+		</div>
+	{/each}
 	<div>총 : {totalPrice}원</div>
 </div>
