c5d83a70909df7fe7b6296a543dcc085b47b06bf
dgkim2008 <dongg9318@gmail.com>
2023-03-14 22:18:53 +0900

    단어 필터링

--- content/tutorial/01-svelte/06-bindings/05-textarea-inputs/단어 필터링/app-b/src/lib/App.svelte
+++ content/tutorial/01-svelte/06-bindings/05-textarea-inputs/단어 필터링/app-b/src/lib/App.svelte
@@ -1,13 +1,32 @@
 <script>
+
+
 	let value = ""
 	let filtered = ""
 
 	const words = ["스벨트", "svelte"]
+	/** 
+	 * @type { (len:number) => string}
+	*/
+	const plus = (len) => {
+		let star = ''
+		for(let i = 0; i < len; i++) {
+			star = star + '*'
+			console.log(star)
+		}
+		console.log(star)
+		return star;
+	}
 
 	$: {
-		
+		filtered = value
+		for(let i = 0; i < 2; i++) {
+			if(filtered.includes(words[i])) {
+				filtered = filtered.replaceAll(words[i], plus(words[i].length))
+			}
+		}
 	}
 </script>
 
-<textarea></textarea>
+<textarea bind:value></textarea>
 <div>{filtered}</div>
