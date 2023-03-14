d8e19479505c940f85a8000760b4bfb5f688e043
dgkim2008 <dongg9318@gmail.com>
2023-03-14 22:30:49 +0900

    학교 학년 선택

--- content/tutorial/01-svelte/06-bindings/06-select-bindings/학교 학년 선택/app-b/src/lib/App.svelte
+++ content/tutorial/01-svelte/06-bindings/06-select-bindings/학교 학년 선택/app-b/src/lib/App.svelte
@@ -1,17 +1,30 @@
 <script>
 	const schools = ["초등학교", "중학교", "고등학교"]
+	const grades = [];
+	
+	for(let i = 0; i < 6; i++) {
+		grades.push(i+1);
+	}
 
 	let selectedSchool
 	let selectedGrade
 </script>
 
-<select>
-	
+<select bind:value={selectedSchool}>
+	{#each schools as school}
+		<option value={school}>
+			{school}
+		</option>
+	{/each}
+</select>
+<select bind:value={selectedGrade}>
+	{#each grades as grade}
+		<option value={grade}>
+			{grade}학년
+		</option>
+	{/each}
 </select>
 
-<select>
-
-</select>
 
 
 <div>{selectedSchool} {selectedGrade}학년</div>
