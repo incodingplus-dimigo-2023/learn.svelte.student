8fadd4c088c1d6cf6adc41547febfb4dfca31047
Haerin <beal2002@naver.com>
2023-03-18 15:36:25 +0900

    학교 학년 선택

--- content/tutorial/01-svelte/06-bindings/06-select-bindings/학교 학년 선택/app-b/src/lib/App.svelte
+++ content/tutorial/01-svelte/06-bindings/06-select-bindings/학교 학년 선택/app-b/src/lib/App.svelte
@@ -3,16 +3,27 @@
 
 	let selectedSchool
 	let selectedGrade
+	let selected
+	let select
+	const school = () =>{
+		selectedSchool=selected
+}
+	const grade = () =>{
+		selectedGrade=select
+}
 </script>
 
-<select>
-	
+<select bind:value={selected} on:click={school}>
+	<option>초등학교</option>
+	<option>중학교</option>
+	<option>고등학교</option>
 </select>
 
-<select>
-
+<select bind:value={select} on:click={grade}>
+	<option>1</option>
+	<option>2</option>
+	<option>3</option>
 </select>
 
 
 <div>{selectedSchool} {selectedGrade}학년</div>
-
