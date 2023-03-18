2beb29adff307a07b4e7c612fb9119abbf9fdecc
dlgudw123 <dlgudwn123123@gmail.com>
2023-03-18 15:49:50 +0900

    숙제 제출

--- content/tutorial/01-svelte/06-bindings/05-textarea-inputs/단어 필터링/app-b/src/lib/App.svelte
+++ content/tutorial/01-svelte/06-bindings/05-textarea-inputs/단어 필터링/app-b/src/lib/App.svelte
@@ -5,9 +5,12 @@
 	const words = ["스벨트", "svelte"]
 
 	$: {
-		
+		filtered = value;
+		for (let i = 0; i < words.length; i++) {
+			filtered = filtered.replaceAll(words[i], '*'.repeat(words[i].length));
+		}
 	}
 </script>
 
-<textarea></textarea>
+<textarea bind:value={value}></textarea>
 <div>{filtered}</div>
