105095afdeaca4432c2052d6cf8b7cbf53916f9d
Junhyeok08 <wnsgur_081106@naver.com>
2023-03-31 02:21:03 +0900

    숙제  제출

--- content/tutorial/01-svelte/06-bindings/05-textarea-inputs/단어 필터링/app-b/src/lib/App.svelte
+++ content/tutorial/01-svelte/06-bindings/05-textarea-inputs/단어 필터링/app-b/src/lib/App.svelte
@@ -5,9 +5,18 @@
 	const words = ["스벨트", "svelte"]
 
 	$: {
+		// filtered = value;
+		// if(value === words[0]){
+		// 	filtered = "***"
+		// } else if(value === words[1]) {
+		// 	filtered = "******"
+		// }
+		console.log(value, filtered)
+
 		
+
 	}
 </script>
 
-<textarea></textarea>
+<textarea bind:value={value}></textarea>
 <div>{filtered}</div>
--- content/tutorial/01-svelte/06-bindings/06-select-bindings/학교 학년 선택/app-b/src/lib/App.svelte
+++ content/tutorial/01-svelte/06-bindings/06-select-bindings/학교 학년 선택/app-b/src/lib/App.svelte
@@ -2,15 +2,35 @@
 	const schools = ["초등학교", "중학교", "고등학교"]
 
 	let selectedSchool
-	let selectedGrade
+	let selectedGrade = '1'
+	$ : {
+		console.log(selectedSchool, selectedGrade)
+	}
+	if(selectedSchool) {
+		selectedGrade = '1'
+	}
 </script>
 
-<select>
-	
+<select bind:value={selectedSchool}>
+	<option>{schools[0]}</option>
+	<option>{schools[1]}</option>
+	<option>{schools[2]}</option>
 </select>
 
-<select>
-
+<select bind:value={selectedGrade}>
+	{#if selectedSchool === '초등학교'}
+		<option>1</option>
+		<option>2</option>
+		<option>3</option>
+		<option>4</option>
+		<option>5</option>
+		<option>6</option>
+	{:else }
+		<option>1</option>
+		<option>2</option>
+		<option>3</option>
+	{/if}
+	
 </select>
 
 
