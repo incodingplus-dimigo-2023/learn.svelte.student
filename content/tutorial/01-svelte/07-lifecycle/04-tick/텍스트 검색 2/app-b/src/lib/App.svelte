32e598928fc78d82cf080a2f613397ae2fa1887c
myrlagksruf <myrlagksruf@gmail.com>
2023-04-02 18:08:24 +0900

    homework 07, 08 추가

---
+++ content/tutorial/01-svelte/07-lifecycle/04-tick/텍스트 검색 2/app-b/src/lib/App.svelte
@@ -0,0 +1,59 @@
+<script>
+	import { afterUpdate, onDestroy, onMount, tick } from "svelte";
+	import { sampleText } from "./sampleText"
+
+	let targetText = ""
+	let result = sampleText
+	
+	let div
+	let input
+
+	let showInput = false
+
+	$: {
+		if(targetText === ""){
+			result = sampleText
+		} else {
+			result = sampleText.replaceAll(targetText, `<span>${targetText}</span>`)
+		}
+	}
+
+	const toggleInputTag = async (event) => {
+		
+	}
+
+	onMount(() => { 
+		
+	})
+
+	onDestroy(() => {
+		
+	})
+</script>
+
+
+{#if showInput}
+	<input type="text" bind:value={targetText} bind:this={input}>	
+{/if}
+
+
+<div bind:this={div}>
+	{@html result}
+</div>
+
+<style>
+	:global(span) {
+		background-color: yellow;
+		font-weight: bold;
+	}
+
+	div {
+		margin-right: 300px;
+	}
+
+	input {
+		position: fixed;
+		top: 0;
+		right: 0;
+	}
+</style>
