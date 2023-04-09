1756661f68bef0aaeb4f935dc7caed9dde3a04a0
JYS <jys60mlb@gmail.com>
2023-04-09 13:31:31 +0900

    Svelte 하이퍼링크 숙제 수정

--- content/tutorial/01-svelte/01-Introduction/03-dynamic-attributes/하이퍼링크/app-b/src/lib/App.svelte
+++ content/tutorial/01-svelte/01-Introduction/03-dynamic-attributes/하이퍼링크/app-b/src/lib/App.svelte
@@ -1,10 +1,10 @@
 <script>
-    const google = "https://www.google.co.kr"
-    const naver = "https://www.naver.com"
-    const incoding = "https://study.in-coding.com"
+    const google = "www.google.co.kr"
+    const naver = "www.naver.com"
+    const incoding = "study.in-coding.com"
 
     const target = "_blank"
 </script>
-<a href={google} {target}>구글</a>
-<a href={naver} {target}>네이버</a>
-<a href={incoding} {target}>인코딩학원</a>
+<a href="https://{google}" {target}>구글</a>
+<a href="https://{naver}" {target}>네이버</a>
+<a href="https://{incoding}" {target}>인코딩학원</a>
