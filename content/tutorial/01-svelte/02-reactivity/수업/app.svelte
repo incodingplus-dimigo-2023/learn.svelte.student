6674954cba3324aa187be10bb4c7f8a356070a99
JYS <jys60mlb@gmail.com>
2023-04-09 14:22:02 +0900

    숫자 추가하기 숫자 빼기

---
+++ content/tutorial/01-svelte/02-reactivity/수업/app.svelte
@@ -0,0 +1,23 @@
+<script>
+	let numbers = [1, 2, 3, 4];
+
+	const addNumber = () => {
+		numbers = [numbers.length + 1,...numbers];
+	}
+
+	$: sum = numbers.reduce((t, n) => t + n, 0);
+
+	const minNumber = () => {
+		numbers = numbers.slice(0,-1)
+	}
+</script>
+
+<p>{numbers.join(' + ')} = {sum}</p>
+
+<button on:click={addNumber}>
+	숫자 추가하기
+</button>
+
+<button on:click={minNumber}>
+	숫자 빼기
+</button>
