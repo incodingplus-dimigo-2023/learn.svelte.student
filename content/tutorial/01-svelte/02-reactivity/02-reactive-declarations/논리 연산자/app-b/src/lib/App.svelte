20f14178f95c6a72ced2538282eedc96a3e85fef
JYS <jys60mlb@gmail.com>
2023-04-14 23:29:45 +0900

    스벨트 2-2 숙제 제출

--- content/tutorial/01-svelte/02-reactivity/02-reactive-declarations/논리 연산자/app-b/src/lib/App.svelte
+++ content/tutorial/01-svelte/02-reactivity/02-reactive-declarations/논리 연산자/app-b/src/lib/App.svelte
@@ -1,16 +1,25 @@
 <script>
 	let a = true;
 	let b = true;
+	$: c = a && b;
+	$: d = a || b;
 
+	const toggleA = () => {
+		a = !a;
+	}
+
+	const toggleB = () => {
+		b = !b;
+	}
 </script>
 
 <div>
-	<div>A: </div>
-	<div>B: </div>
+	<div on:click={toggleA}>A: {a}</div>
+	<div on:click={toggleB}>B: {b}</div>
 </div>
 <div>
-	<div>A && B: </div>
-	<div>A || B: </div>
+	<div>A && B: {c}</div>
+	<div>A || B: {d}</div>
 </div>
 
 <style>
