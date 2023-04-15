4d360aacc4833b061e21a6acc901bb907881246a
JYS <jys60mlb@gmail.com>
2023-04-15 21:47:08 +0900

    스벨트 2-4 숙제 제출

--- content/tutorial/01-svelte/02-reactivity/04-updating-arrays-and-objects/소수 구하기/app-b/src/lib/App.svelte
+++ content/tutorial/01-svelte/02-reactivity/04-updating-arrays-and-objects/소수 구하기/app-b/src/lib/App.svelte
@@ -1,23 +1,104 @@
 <script>
-	let arr = []
-	const MAX = 100
+	let arr = [];
+	const MAX = 100;
+
+	let isRunning = false;
+	let isAlreadyCalc = false;
 
 	const generate = () => {
-		arr = []
+		if(isRunning === true){
+			return;
+		}
 
+		isRunning = true;
+		arr = [];
+		
 		let i = 1;
 		const interval = setInterval(() => {
-		
-		}, 100)
+			arr = [...arr, i];
+
+			if(i === MAX){
+				clearInterval(interval);
+				isRunning = false;
+				isAlreadyCalc = false;
+				return;
+			}
+
+			i += 1;
+		}, 100);
 	}
 
 	const calculate = () => {
-		arr = [...arr.slice(1)]
+		if(isRunning === true || isAlreadyCalc === true){
+			return;
+		}
+		
+		isRunning = true;
+		arr = [...arr.slice(1)];
+
+		let value = 2;	//	2 ~ 100 까지 검사할때 활용한다.
+		const interval = setInterval(() => {
+			// Interval 마다 숫자를 최대 1개 제거 하는 방식
+
+			// value 가 소수라면 arr 에서 value 가 있는지 찾고 제거한다.
+			for(let i = 2; i < (value - 1); i++){
+				if(value % i === 0){
+					let index = arr.indexOf(value);
+					if(index !== -1){
+						// arr 에서 index 번째를 제거 한다.
+						arr = [...arr.slice(0,index), ...arr.slice(index+1)];
+					}
+					break;
+				}
+			}
+
+			value++;
+
+			if(value > MAX){
+				clearInterval(interval);
+				isRunning = false;
+				isAlreadyCalc = true;
+				return;
+			}
+		}, 100)
+	}
+
+	const calculate2 = () => {
+		if(isRunning === true || isAlreadyCalc === true){
+			return;
+		}
+		
+		isRunning = true;
+		arr = [...arr.slice(1)];
 
 		let i = 2;
 		const interval = setInterval(() => {
+			// 에라토스테네스의 체 방식
+			// arr : 2 ~ 100
+
+			// 2의 배수 제거, 3의 배수 제거 ... 50의 배수 제거
+			// 배수 제거 시 자기 자신보다 큰 수만 검사
+			let brr = [];
 			
-		}, 100)
+			while(arr.length > 0){
+				let number = arr.shift();
+				if( number <= i || number % i !== 0 ){
+					brr.push(number);
+				}
+			}
+
+			i++;
+
+			arr = brr;
+
+			if( i > MAX/2){
+				clearInterval(interval);
+				isRunning = false;
+				isAlreadyCalc = true;
+				return;
+			}
+
+		}, 100);
 	}
 
 </script>
