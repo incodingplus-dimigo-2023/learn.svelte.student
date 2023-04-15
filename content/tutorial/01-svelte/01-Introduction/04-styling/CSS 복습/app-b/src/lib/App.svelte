0d59a94f71db4edcd8f6080280cb02091fc6b242
sion <cionsglory@gmail.con>
2023-04-16 00:25:31 +0900

    스벨트 첫번째 숙제 변경

--- content/tutorial/01-svelte/01-Introduction/04-styling/CSS 복습/app-b/src/lib/App.svelte
+++ content/tutorial/01-svelte/01-Introduction/04-styling/CSS 복습/app-b/src/lib/App.svelte
@@ -1,6 +1,6 @@
 <div class="container">
 	<h1>Hello, nest!</h1>
-	<div>
+	<div class="font">
 		A progressive Node.js framework for building efficient, reliable and scalable server-side
 		applications.
 	</div>
@@ -12,11 +12,21 @@
 
 <style>
 	.container {
-	
+	width: 100%;
+	height: 100%;
+		background-color: black;
+		color: white;
+		line-height: 50px;
+		padding-left: 50px;
+		padding-top: 30px;
+		}
+	.font{
+		font-size: 16.7px;
+		font-weight: 5px;
+		line-height: 20px;
 	}
-
 	.btn-container {
-		
+		padding-top: 25px;
 	}
 
 	button {
@@ -31,16 +41,21 @@
 	}
 
 	.doc {
-
+	color: white;
+	background-color: #ea2845;
 	}
 	.doc:hover {
-
+	border: 2px solid #ea2845;
+	background-color: black;
 	}
 
 	.git {
-	
+	border: 2px solid white;
+	background-color: black;
 	}
 	.git:hover {
-		
+	border: 2px solid #ea2845;
+	color: #ea2845;
 	}
 </style>
+
