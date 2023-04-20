754e1cd91aae6df865aac35aadd32216889148ef
JYS <jys60mlb@gmail.com>
2023-04-20 18:12:59 +0900

    스벨트 3-2 숙제 제출

--- content/tutorial/01-svelte/03-props/02-default-values/CSS 기본 속성/app-b/src/lib/App.svelte
+++ content/tutorial/01-svelte/03-props/02-default-values/CSS 기본 속성/app-b/src/lib/App.svelte
@@ -1,4 +1,7 @@
 <script>
 	import Component from "./Component.svelte";
 </script>
-
+<Component />
+<Component backgroundcolor = "background-color: blue;" />
+<Component backgroundcolor = "background-color: blue;" color = "color: red;"/>
+<Component backgroundcolor = "background-color: blue;" color = "color: red;" padding = "padding: 100px;"/>
--- content/tutorial/01-svelte/03-props/02-default-values/CSS 기본 속성/app-b/src/lib/Component.svelte
+++ content/tutorial/01-svelte/03-props/02-default-values/CSS 기본 속성/app-b/src/lib/Component.svelte
@@ -1,14 +1,16 @@
 <script>
-
+    export let backgroundcolor = 'background-color: transparent;';
+    export let color = 'color: canvastext;';
+    export let padding = 'padding: 0;';
 </script>
 
 <div class="container">
     <div>
-        <div></div>
-        <div></div>
-        <div></div>
+        <div>{backgroundcolor}</div>
+        <div>{color}</div>
+        <div>{padding}</div>
     </div>
-    <div class="view">텍스트</div>
+    <div class="view" style="{backgroundcolor} {color} {padding}">텍스트</div>
 </div>
 
 <style>
