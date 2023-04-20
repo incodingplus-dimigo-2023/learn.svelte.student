3c4abb43676aa951c211be9e3c3640092ca162d4
JYS <jys60mlb@gmail.com>
2023-04-20 21:53:31 +0900

    스벨트 4-2 숙제 제출

--- content/tutorial/01-svelte/04-logic/02-else-blocks/로그인/app-b/src/lib/App.svelte
+++ content/tutorial/01-svelte/04-logic/02-else-blocks/로그인/app-b/src/lib/App.svelte
@@ -1,12 +1,19 @@
 <script>
-	let isTeacherMode = false
+	let isTeacherMode = false;
+	const togglemode = () => {
+		isTeacherMode = !isTeacherMode;
+	};
 </script>
+	
+{#if isTeacherMode}
+	<input type="password" placeholder="선생님 코드를 입력해주세요">
+{:else}
+	<input type="text" placeholder="아이디를 입력해주세요">
+	<input type="password" placeholder="비밀번호를 입력해주세요">
+{/if}
 
-<input type="password" placeholder="선생님 코드를 입력해주세요">	
-<input type="text" placeholder="아이디를 입력해주세요">
-<input type="password" placeholder="비밀번호를 입력해주세요">	
-<div>
-	선생님 모드: 
+<div on:click={togglemode}>
+	선생님 모드: {isTeacherMode}
 </div>
 
 <style>
