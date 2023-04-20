48127dfc0acee74af40a83860712afae7bf4e3dc
dog898996 <dog898996@gmail.com>
2023-04-20 22:16:41 +0900

    changed

--- content/tutorial/01-svelte/07-lifecycle/02-ondestroy/타이머 가속/app-b/src/lib/Time.svelte
+++ content/tutorial/01-svelte/07-lifecycle/02-ondestroy/타이머 가속/app-b/src/lib/Time.svelte
@@ -1,14 +1,15 @@
 <script>
-	import { createEventDispatcher, onDestroy, onMount } from "svelte";
+    import { createEventDispatcher, onDestroy, onMount } from "svelte";
     let dis = createEventDispatcher();
     let inter;
     onMount(() => {
         inter = setInterval(() => {
-            dis('time');
+            dis("time");
         }, 1000);
     });
     onDestroy(() => {
-        // setInterval 지우는 코드 작성
+        clearInterval(inter);
     });
 </script>
+
 <button on:click>가속 지우기</button>
