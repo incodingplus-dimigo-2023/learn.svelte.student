3ec299067a8dcd2fad2acf96c2c0bc7ab319e1e8
Leejiwoo0703 <Leejiwoo0703>
2023-04-22 16:14:40 +0900

    숙제 완료

--- content/tutorial/01-svelte/01-Introduction/02-your-first-component/JS 복습/app-b/src/lib/App.svelte
+++ content/tutorial/01-svelte/01-Introduction/02-your-first-component/JS 복습/app-b/src/lib/App.svelte
@@ -1,9 +1,21 @@
 <script>
     let str = 'hello world';
-</script>
+    let to = str.split('')
+    for(let i = 0; i < to.length; i++ ){
+        if(to[i] === 'l'){
+            to[i] = '1'
+        }
+    }
+    let result = []
+    for(let i = 1; i < 101; i++){
+        if(i % 7 === 0){
+            result.push(i);
+        }
+    }
 
+</script>
 <ol>
-    <li>12345 + 54321 * 12345 / 54321 의 답은 ? 입니다.</li>
-    <li>hello wolrd에서 l을 모두 1로 바꾼 문자열은 ? 입니다.</li>
-    <li>1에서 100까지 7의 배수는 ? 입니다.</li>
+    <li>12345 + 54321 * 12345 / 54321 의 답은 {12345 + 54321 * 12345 / 54321} 입니다.</li>
+    <li>hello wolrd에서 l을 모두 1로 바꾼 문자열은 {to.join('')} 입니다.</li>
+    <li>1에서 100까지 7의 배수는 {result} 입니다.</li>
 </ol>
