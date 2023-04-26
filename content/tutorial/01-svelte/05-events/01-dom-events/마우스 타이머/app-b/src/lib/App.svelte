96af4b2fc5d58f8fad9eddb3523247f282e8fae2
ksoeun6204 <ksoeun6204@naver.com>
2023-04-26 21:42:10 +0900

    svelte.과녁 수정

--- content/tutorial/01-svelte/05-events/01-dom-events/마우스 타이머/app-b/src/lib/App.svelte
+++ content/tutorial/01-svelte/05-events/01-dom-events/마우스 타이머/app-b/src/lib/App.svelte
@@ -1,9 +1,10 @@
 <script>
-	let time = 0
-	let isMouseDown = false
+	let time = 0;
+	let isMouseDown = false;
+	let setT;
 </script>
 
-<div>
+<div on:mousedown={()=>{isMouseDown = true; time = 0; setT = setInterval(()=>{time += 1;},1)}} on:mouseup={()=>{isMouseDown = false; clearInterval(setT)}}>
 	{isMouseDown ? "마우스를 누르는 중입니다." : `총 ${time / 1000}초 눌렀습니다.`}
 </div>
 
