52460f9c000d411d3499e1ae184fdd5d4e1af11a
고은찬 <ssssaaaa50205020@gmail.com>
2023-04-26 22:09:21 +0900

    02-01제출

--- content/tutorial/01-svelte/02-reactivity/01-reactive-assignments/타이머/app-b/src/lib/App.svelte
+++ content/tutorial/01-svelte/02-reactivity/01-reactive-assignments/타이머/app-b/src/lib/App.svelte
@@ -1,25 +1,37 @@
 <script>
 	let number = 5;
-	let interval = null;
+	let interval = () => {
+		if(number == 0){
+			console.log('끝났습니다')
+		}
+		else{
+					number = number -1;
+		}
 
+		};
+
+			let sta;
 	const reset = () => {
-		
+				number = 5
+		clearInterval(sta)
 	};
 	const pause = () => {
-		
+		clearInterval(sta)
 	};
 	const start = () => {
-		
+		clearInterval(sta)
+		sta = setInterval(interval, 1000)
 	};
+
 </script>
 
 <div>
 	<div class="number">{number}</div>
 </div>
 <div>
-	<button>시작</button>
-	<button>일시 정지</button>
-	<button>초기화</button>
+	<button on:click={start}>시작</button>
+	<button on:click={pause}>일시 정지</button>
+	<button on:click={reset}>초기화</button>
 </div>
 
 <style>
@@ -27,3 +39,4 @@
 		font-size: 100px;
 	}
 </style>
+
