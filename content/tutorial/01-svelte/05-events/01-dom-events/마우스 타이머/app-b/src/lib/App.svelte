65169cf21b229ca39df726494fb1675d9b365352
JYS <jys60mlb@gmail.com>
2023-04-28 00:06:24 +0900

    스벨트 5-1 숙제 제출

--- content/tutorial/01-svelte/05-events/01-dom-events/마우스 타이머/app-b/src/lib/App.svelte
+++ content/tutorial/01-svelte/05-events/01-dom-events/마우스 타이머/app-b/src/lib/App.svelte
@@ -1,9 +1,23 @@
 <script>
-	let time = 0
-	let isMouseDown = false
+	let time = 0;
+	let isMouseDown = false;
+	let interval = null;
+	
 </script>
 
-<div>
+<div on:mousedown={() => {
+		time = 0;
+		isMouseDown = true; 
+		//console.log("MouseDown");
+
+		interval = setInterval(() => {
+			time += 4;
+		},4);
+	}} 
+	on:mouseup={() => {
+		clearInterval(interval);
+		isMouseDown = false;
+	}}>
 	{isMouseDown ? "마우스를 누르는 중입니다." : `총 ${time / 1000}초 눌렀습니다.`}
 </div>
 
