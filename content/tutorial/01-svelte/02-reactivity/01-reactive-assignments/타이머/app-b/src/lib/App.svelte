fd973602874dc3c569a63a4bc6b4ea5776d71d2f
Leejiwoo0703 <Leejiwoo0703>
2023-04-29 15:28:34 +0900

    타이머 숙제 완료

--- content/tutorial/01-svelte/02-reactivity/01-reactive-assignments/타이머/app-b/src/lib/App.svelte
+++ content/tutorial/01-svelte/02-reactivity/01-reactive-assignments/타이머/app-b/src/lib/App.svelte
@@ -1,25 +1,40 @@
 <script>
 	let number = 5;
 	let interval = null;
-
+	let play = 0;
 	const reset = () => {
-		
+		clearInterval(interval);
+		number = 5;
+		play = 0;
 	};
 	const pause = () => {
-		
+		if (play === 1){
+			clearInterval(interval);
+			play = 0;
+		}
 	};
 	const start = () => {
-		
+		if (play === 0){
+			interval = setInterval(print, 1000);
+			play = 1;
+		};
 	};
+	let print = () => {
+		if(number > 0){
+			number--;
+		};
+	};
+	
+	
 </script>
 
 <div>
 	<div class="number">{number}</div>
 </div>
 <div>
-	<button>시작</button>
-	<button>일시 정지</button>
-	<button>초기화</button>
+	<button on:click={start}>시작</button>
+	<button on:click={pause}>일시 정지</button>
+	<button on:click={reset}>초기화</button>
 </div>
 
 <style>
