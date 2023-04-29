4c5afcafef74a096d379a02fd01436375c8a811d
Leejiwoo0703 <Leejiwoo0703>
2023-04-29 15:54:44 +0900

    구구단 숙제 완료

--- content/tutorial/01-svelte/02-reactivity/03-reactive-statements/구구단/app-b/src/lib/App.svelte
+++ content/tutorial/01-svelte/02-reactivity/03-reactive-statements/구구단/app-b/src/lib/App.svelte
@@ -1,13 +1,21 @@
 <script>
 	let number = 0;
 	let multiplicationTable = "";
-
+	
 	$: {
-		
+
 	}
 
 	const setNumber = (num) => {
 		number = num
+		let number2 = 0;
+		document.querySelector('.container').innerHTML = ''
+		for(let i = 0; i < 9; i++){
+			number2 += 1
+			multiplicationTable += `${number} * ${number2} = ${number * number2}`
+			document.querySelector('.container').innerHTML += `<div>${number} * ${number2} = ${number * number2}</div>`
+			document.querySelector('.container').innerHTML += `<div>\n</div>`
+		}
 	}
 </script>
 
@@ -16,4 +24,4 @@
 	<button on:click={() => setNumber(6)}>6단</button>
 	<button on:click={() => setNumber(9)}>9단</button>
 </div>
-<div></div>
+<div class="container">{multiplicationTable}</div>
