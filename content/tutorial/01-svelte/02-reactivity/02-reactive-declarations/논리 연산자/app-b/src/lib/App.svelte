4ec80e55ed382e7bc12f61dd0867063ffb5c9411
Leejiwoo0703 <Leejiwoo0703>
2023-04-29 15:37:57 +0900

    논리 연산자 숙제 완료

--- content/tutorial/01-svelte/02-reactivity/02-reactive-declarations/논리 연산자/app-b/src/lib/App.svelte
+++ content/tutorial/01-svelte/02-reactivity/02-reactive-declarations/논리 연산자/app-b/src/lib/App.svelte
@@ -1,16 +1,24 @@
 <script>
 	let a = true;
 	let b = true;
-
+	
+	const a_click= () => {
+		a = !a
+	}
+	const b_click = () => {
+		b = !b
+	}
 </script>
 
 <div>
-	<div>A: </div>
-	<div>B: </div>
+	<!-- svelte-ignore a11y-click-events-have-key-events -->
+	<div on:click = {a_click}>A:{a} </div>
+	<!-- svelte-ignore a11y-click-events-have-key-events -->
+	<div on:click = {b_click}>B:{b} </div>
 </div>
 <div>
-	<div>A && B: </div>
-	<div>A || B: </div>
+	<div>A && B:{a && b} </div>
+	<div>A || B:{a || b} </div>
 </div>
 
 <style>
