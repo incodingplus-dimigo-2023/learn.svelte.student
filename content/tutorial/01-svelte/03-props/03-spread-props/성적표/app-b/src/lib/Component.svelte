3da6ad968770bd7a0edc5da0fbab03136a483437
고은찬 <ssssaaaa50205020@gmail.com>
2023-05-14 13:38:28 +0900

    제출

--- content/tutorial/01-svelte/03-props/03-spread-props/성적표/app-b/src/lib/Component.svelte
+++ content/tutorial/01-svelte/03-props/03-spread-props/성적표/app-b/src/lib/Component.svelte
@@ -3,43 +3,52 @@
     export let korean;
     export let math;
     export let english;
-
-    const getGrade = (score) => {
-      
+  
+    const Grade = (score) => {
+      if (score >= 90) {
+        return 'A';
+      } else if (score >= 80 && score < 90) {
+        return 'B';
+      } else if (score >= 70 && score < 80) {
+        return 'C';
+      } else if (score >= 60 && score < 70) {
+        return 'D';
+      } else {
+        return 'F';
+      }
     }
-</script>
-
-<div>
+  </script>
+  
+  <div>
     <h1>{name}님의 성적표</h1>
     <table>
-        <tr>
-            <td>과목</td>
-            <td>국어</td>
-            <td>수학</td>
-            <td>영어</td>
-        </tr>
-        <tr>
-            <td>점수</td>
-            <td></td>
-            <td></td>
-            <td></td>
-        </tr>
-        <tr>
-            <td>등급</td>
-            <td></td>
-            <td></td>
-            <td></td>
-        </tr>
+      <tr>
+        <td>과목</td>
+        <td>국어</td>
+        <td>수학</td>
+        <td>영어</td>
+      </tr>
+      <tr>
+        <td>점수</td>
+        <td>{korean}</td>
+        <td>{math}</td>
+        <td>{english}</td>
+      </tr>
+      <tr>
+        <td>등급</td>
+        <td>{Grade(korean)}</td>
+        <td>{Grade(math)}</td>
+        <td>{Grade(english)}</td>
+      </tr>
     </table>
-</div>
-
-<style>
+  </div>
+  
+  <style>
     table {
-        border-collapse: collapse;
-        text-align: center;
+      border-collapse: collapse;
+      text-align: center;
     }
     td {
-        border: 1px solid black;
+      border: 1px solid black;
     }
-
-</style>
+  </style>
--- content/tutorial/01-svelte/05-events/01-dom-events/마우스 타이머/app-b/src/lib/App.svelte
+++ content/tutorial/01-svelte/05-events/01-dom-events/마우스 타이머/app-b/src/lib/App.svelte
@@ -1,10 +1,30 @@
 <script>
 	let time = 0
-	let isMouseDown = false
+	let click = false
 </script>
 
-<div>
-	{isMouseDown ? "마우스를 누르는 중입니다." : `총 ${time / 1000}초 눌렀습니다.`}
+<div 
+	on:mousedown={() => {
+		click = true
+		 let startTime = Date.now()
+		
+		const timer = setInterval(() => {
+			if(click) {
+				time = Date.now() - startTime
+			} else {
+				clearInterval(timer)
+			}
+		}, 10)
+		
+	}}
+	on:mouseup={() => {
+		click = false
+	}}
+	on:mouseleave={() => {
+		click = false
+	}}
+>
+	{click ? "마우스를 누르는 중입니다." : `총 ${time / 1000}초 눌렀습니다.`}
 </div>
 
 <style>
